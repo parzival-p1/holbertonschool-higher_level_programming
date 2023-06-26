e264f40ff74e9a1e599505c0788095eec82a2c27
Paco Hdez <francj.hdez@gmail.com>
2023-06-25 22:46:02 -0600

    Technical interview preparation:

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    mapper = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    result = 0
+    table = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
 
-    for idx in range(0, len(roman_string)):
-        if idx == len(roman_string) - 1:
-            result += mapper[roman_string[idx]]
-            break
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    prev, sum = 0, 0
 
-        if mapper[roman_string[idx]] < mapper[roman_string[idx + 1]]:
-            result -= mapper[roman_string[idx]]
-        else:
-            result += mapper[roman_string[idx]]
-
-    return result
+    for c in roman_string:
+        sum += table[c] if table[c] <= prev else table[c] - prev * 2
+        prev = table[c]
+    return sum
