d541952faa9ece5e371dbde378df2123dec4165a
Paco Hdez <francj.hdez@gmail.com>
2023-06-25 21:51:46 -0600

    replaces an element of a list at a specific position

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    return (my_list[idx] if 0 <= idx < len(my_list) else None)
+    items = len(my_list) - 1
+
+    if idx < 0 or idx > items:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
