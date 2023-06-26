1479f44b9c43f97b15d8e11cce8a9d23fbcb266a
Paco Hdez <francj.hdez@gmail.com>
2023-06-25 20:51:21 -0600

    program that prints the number of and the list of its arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     leng = len(argv)
-    print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
+    print("{:d} {:s}{:s}".format(leng - 1, "argument" if leng <= 2 else "arguments",
                                  "." if leng == 1 else ":"))
     for i, s in enumerate(argv):
         if i > 0:
