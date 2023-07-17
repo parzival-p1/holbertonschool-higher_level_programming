ee3244913400ef8a30ea9ed3884f2f21bfe26104
Paco Hdez <francj.hdez@gmail.com>
2023-07-16 20:47:59 -0600

    function that returns True if the object is exactly an instance of the specified class ; otherwise False

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+
+
 """
 This module has a class MyList that inherits from
 list. Has a Public instance method print_sorted that
 prints the list in ascending order
 """
 
+
 class MyList(list):
-
-
     """a subclass of list"""
     def __init__(self):
         """Initializes the super object"""
