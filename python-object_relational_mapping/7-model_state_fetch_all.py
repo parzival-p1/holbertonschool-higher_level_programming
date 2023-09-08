e3963e38cfc91cf0e66d7f797d6d1c49d7c11316
Paco Hdez <francj.hdez@gmail.com>
2023-09-08 15:56:37 -0600

    script that lists all State objects from the database hbtn_0e_6_usa

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""
-script that lists all state objects from the
-db takes 3 args result needs to be sorted
-"""
+""" lists all State objects from the database hbtn_0e_6_usa """
+
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+
 if __name__ == "__main__":
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-    session = sessionmaker(bind=engine)
-    Base.metadata.create_all(engine)
-    for st in session().query(State).order_by(State.id):
-        print("{}: {}".format(st.id, st.name))
-    session().close()
+
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
