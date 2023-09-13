46c18748d4c82067cf37228e77119f7cb86598e2
Paco Hdez <francj.hdez@gmail.com>
2023-09-13 14:11:58 -0600

    prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for char in range(122, 96, -1):
-    print(chr(char) if char % 2 == 0 else chr(char-32), end="")
+    print("{:c}".format(char if char % 2 == 0 else (char-32)), end="")
